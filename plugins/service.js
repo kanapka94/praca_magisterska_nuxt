d532a4ef023ba1f14ba14b1307da7899b0f6cac9
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-16 15:06:59 +0200

    Add services

--- plugins/service.js
+++ plugins/service.js
@@ -1,6 +1,19 @@
 const createService = axios => ({
-  home: {},
-  search: {}
+  getList: {
+    alcohol: () => axios.$get(`/list.php?a=list`),
+    categories: () => axios.$get(`/list.php?c=list`),
+    glasses: () => axios.$get(`/list.php?g=list`),
+    ingredients: () => axios.$get(`/list.php?i=list`)
+  },
+  filter: {
+    alcohol: payload => axios.$get(`/filter.php?a=${payload}`),
+    categories: payload => axios.$get(`/filter.php?c=${payload}`),
+    glasses: payload => axios.$get(`/filter.php?g=${payload}`),
+    ingredients: payload => axios.$get(`/filter.php?i=${payload}`)
+  },
+  drinks: {
+    getRandom: () => axios.$get(`/random.php`)
+  }
 })
 
 export default ({ app }, inject) => {
