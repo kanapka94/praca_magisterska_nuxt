bdf78747e01e94128af9b9dd2d22d7b0bf99b1b5
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-16 14:15:26 +0200

    Run prettier inside VSCode

--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,18 @@
   <section class="container">
     <div>
       <logo />
-      <h1 class="title">
-        praca_magisterska_nuxt
-      </h1>
-      <h2 class="subtitle">
-        Projekt pracy magisterskiej
-      </h2>
+      <h1 class="title">praca_magisterska_nuxt</h1>
+      <h2 class="subtitle">Projekt pracy magisterskiej</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue"
 
 export default {
   components: {
@@ -45,8 +33,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
