920b9b809e25af5867d77cfd288d043222ede435
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-17 23:31:56 +0200

    Working on burger menu

---
+++ components/general/TheMenu/Burger.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="burger" @click="handleClick">
+    <input type="checkbox" class="checkbox">
+    <span/>
+    <span/>
+    <span/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Burger',
+    data () {
+      return {
+        open: false
+      }
+    },
+    methods: {
+      handleClick () {
+        this.open = !this.open
+        this.$emit('click', this.open)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .burger {
+    position: relative;
+  }
+
+  .checkbox {
+    position: absolute;
+    z-index: 2;
+    width: 40px;
+    height: 32px;
+    display: block;
+    opacity: 0;
+    cursor: pointer;
+    -webkit-touch-callout: none;
+
+    &:checked ~ span {
+      background-color: #232323;
+      opacity: 1;
+      transform: rotate(45deg) translate(-2px, -1px);
+
+      &:nth-last-child(2) {
+        transform: rotate(-45deg) translate(0, -1px);
+      }
+
+      &:nth-last-child(3) {
+        opacity: 0;
+        transform: rotate(0deg) scale(0.2, 0.2);
+      }
+    }
+  }
+
+  span {
+    position: relative;
+    top: 10px;
+    right: 10px;
+    width: 33px;
+    height: 4px;
+    margin-bottom: 5px;
+    display: block;
+
+    background: #abcabc;
+    border-radius: 3px;
+    transform-origin: 4px 0;
+    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+      background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+      opacity 0.55s ease;
+
+    &:first-child {
+      transform-origin: 0 0;
+    }
+
+    &:nth-last-child(2) {
+      transform-origin: 0 100%;
+    }
+  }
+</style>
---
+++ components/general/TheMenu/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <burger/>
+  </div>
+</template>
+
+<script>
+  import Burger from './Burger'
+  export default {
+    name: 'TheMenu',
+    components: { Burger }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: flex-end; // todo: finish burger
+  }
+
+  .burger {
+    right: 10px;
+  }
+</style>
